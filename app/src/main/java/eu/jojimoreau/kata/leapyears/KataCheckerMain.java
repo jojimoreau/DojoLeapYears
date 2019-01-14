21cf2a82d7f9da5be860f546d9d4831710aca9f6
Joji Moreau <jojimoreau@gmail.com>
2019-01-14 17:55:50 +0100

    Creation of the project

---
+++ app/src/main/java/eu/jojimoreau/kata/leapyears/KataCheckerMain.java
@@ -0,0 +1,13 @@
+package eu.jojimoreau.kata.leapyears;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KataCheckerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kata_checker_main);
+    }
+}
