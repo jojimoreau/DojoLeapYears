91f67a81eb7183d14bbd22b685264ffc48a14fcb
Joji Moreau <jojimoreau@gmail.com>
2019-01-15 22:47:10 +0100

    Implementation of the leapCheck() logic

--- app/src/main/java/eu/jojimoreau/kata/leapyears/KataCheckerMain.java
+++ app/src/main/java/eu/jojimoreau/kata/leapyears/KataCheckerMain.java
@@ -12,6 +12,26 @@ public class KataCheckerMain extends AppCompatActivity {
     }
 
     protected boolean leapCheck(int year){
-        return true;
+        // All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).-->
+        if((year % 4 != 0)){
+            return false;
+        }
+
+        // All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+        if(year % 400 == 0){
+            return true;
+        }
+
+        // All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+        if((year % 4 == 0) && (year % 100 != 0)){
+            return true;
+        }
+
+        // All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+        if((year % 100 == 0) && (year % 400 != 0)){
+            return false;
+        }
+
+        return false;
     }
 }
